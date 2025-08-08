7cd19f2e77393bbe5ea03a751061441727f04782
AcaUrosevic <aleksandar.urosevic@fonis.rs>
2025-08-08 13:32:02 +0200

    frontend setup

--- backend/app/Http/Controllers/ReportController.php
+++ backend/app/Http/Controllers/ReportController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
-    //
+    public function userWorkHours()
+    {
+        $report = DB::table('time_entries')
+            ->join('users', 'time_entries.user_id', '=', 'users.id')
+            ->select(
+                'users.id as user_id',
+                'users.name',
+                'users.position',
+                DB::raw('SUM(time_entries.hours) as total_hours')
+            )
+            ->groupBy('users.id', 'users.name', 'users.position')
+            ->get();
+
+        return response()->json($report);
+    }
 }
